8db0ec37d18a4290d13fb40d738e0ab5debb877b
sumannandi375 <od141454@gmail.com>
2025-09-15 15:48:13 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+        
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(int num) {
+        int originalNum = num;
+        int reversedNum = 0;
+        
+        // Reverse the number
+        while (num != 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+        
+        // Check if original and reversed are the same
+        return originalNum == reversedNum;
+    }
+}
